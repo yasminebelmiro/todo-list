ce50f0f3329127e425fa9fbf6be2d097c35b19c4
Yasmine Belmiro <devyasminebelmiro@gmail.com>
2026-04-09 07:35:39 -0300

    feat: first commit

---
+++ src/test/java/com/yasminebelmiro/todo_list/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasminebelmiro.todo_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
